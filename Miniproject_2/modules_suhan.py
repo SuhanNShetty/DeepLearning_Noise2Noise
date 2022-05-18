7e9497761b40c3f055bb985e4a9924a2d30fac76
SuhanNshetty <suhan.n.shetty@gmail.com>
2022-05-18 20:30:31 +0200

    updates to Sequential module (still incomplete)

--- Miniproject_2/modules_suhan.py
+++ Miniproject_2/modules_suhan.py
@@ -1,8 +1,7 @@
 import torch
-
 class SGD:
 	'''
-		Gets a tuple of ((weight,grad_weight),(bias,grad_bias)) and updates the learning parameters
+		Updates the learning parameters
 		Disclaimer: We used https://github.com/pytorch/pytorch/blob/cd9b27231b51633e76e28b6a34002ab83b0660fc/torch/optim/sgd.py#L63
 		as reference for writing our version
 	'''
@@ -30,12 +29,13 @@ class SGD:
 			Set gradients to zero
 		'''
 		for tfm in self.transforms:
-			tfm.params[0][1] = 0*tfm.params[0][1]
-			tfm.params[1][1] = 0*tfm.params[1][1]
+			tfm.params[0][1].fill_(0.)
+			tfm.params[1][1].fill_(0)
 
 	def step(self):
 		'''
 			Take one gradient step 
+			To Do: Check if the original 
 		'''
 		for i,tfm in enumerate(self.transforms):
 			# set momentum
@@ -45,8 +45,8 @@ class SGD:
 			tfm.params[0][0] -= self.lr*self.velocity[i][0] # weight
 			tfm.params[1][0] -= self.lr*self.velocity[i][1] # bias
 			# zero the grad:
-			tfm.params[0][1] = 0*tfm.params[0][1] # weight
-			tfm.params[1][1] = 0*tfm.params[1][1] # bias
+			tfm.params[0][1].fill_(0.) #weight
+			tfm.params[1][1].fill_(0.) # bias
 
 
 class Sequential:
@@ -56,8 +56,7 @@ class Sequential:
 
 	def forward(self, x, params):
 		for tfm in self.transforms:
-			tfm.set_params(tfm.params[0][0])
-			x = tfm.forward(x,weight=tfm.params[0][0],bias=tfm.params[1][0])
+			x = tfm.forward(x)
 			self.params
 		return x
 
@@ -78,12 +77,12 @@ class LinearLayer:
 		y = xA' + b
 	'''
 	def __init__(self, in_features, out_features, bias=True, device=None):
-		self.weight = torch.empty(in_features,out_features).randn_().to(device)
-		self.bias = torch.zeros((1,out_features)).to(device)
+		self.weight = torch.empty(1,in_features,out_features).randn_().to(device)
+		self.bias = torch.zeros((1,1,out_features)).to(device)
 		self.params = ((self.weight,0*self.weight),(self.bias,0*self.bias))
 
 	def forward(self,x):
-		y = x@self.weight.flip(0,1) + self.bias
+		y = x@self.weight.flip(1,2) + self.bias
 		return y 
 
 	def backward(self,grad_param_next):
