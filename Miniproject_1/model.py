c7da08c473964066696288f100d8047df301611f
fcentamo <frank.centamori@epfl.ch>
2022-05-22 13:57:06 +0200

    Implementation project 1 nearly done

--- Miniproject_1/model.py
+++ Miniproject_1/model.py
@@ -1,91 +1,96 @@
-import torch
-from torch.profiler import profile, record_function, ProfilerActivity
+import torch 
 from torch import nn
 from tqdm import tqdm
 
-
-class Model():
-    def __init__(self) -> None:
-        ## instantiate model + optimizer + loss function + any other stuff you need
-
-        # Check the device to use
-        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
-        # Define the number of in, middle and out channels
-        in_channels = 3
-        m = 10
-        out_channels = 3
-
-        # Initialize the model
-        # Layers: enc_conv0, enc_conv1, pool1
-        self._block1 = nn.Sequential(
-            nn.Conv2d(in_channels, m, 3, stride=1, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(m, m, 3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(2))
-
-        # Layers: enc_conv(i), pool(i); i=2..5
-        self._block2 = nn.Sequential(
-            nn.Conv2d(m, m, 3, stride=1, padding=1),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(2))
-
-        # Layers: enc_conv6, upsample5
-        self._block3 = nn.Sequential(
-            nn.Conv2d(m, m, 3, stride=1, padding=1),
-            nn.ReLU(inplace=True),
-            nn.ConvTranspose2d(m, m, 3, stride=2, padding=1, output_padding=1))
-            #nn.Upsample(scale_factor=2, mode='nearest'))
-
-        # Layers: dec_conv5a, dec_conv5b, upsample4
-        self._block4 = nn.Sequential(
-            nn.Conv2d(m * 2, m * 2, 3, stride=1, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(m*2, m*2, 3, stride=1, padding=1),
-            nn.ReLU(inplace=True),
-            nn.ConvTranspose2d(m*2, m*2, 3, stride=2, padding=1, output_padding=1))
-            #nn.Upsample(scale_factor=2, mode='nearest'))
-
-        # Layers: dec_deconv(i)a, dec_deconv(i)b, upsample(i-1); i=4..2
-        self._block5 = nn.Sequential(
-            nn.Conv2d(m*3, m*2, 3, stride=1, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(m*2, m*2, 3, stride=1, padding=1),
-            nn.ReLU(inplace=True),
-            nn.ConvTranspose2d(m*2, m*2, 3, stride=2, padding=1, output_padding=1))
-            #nn.Upsample(scale_factor=2, mode='nearest'))
-
-        # Layers: dec_conv1a, dec_conv1b, dec_conv1c,
-        self._block6 = nn.Sequential(
-            nn.Conv2d(m*2 + in_channels, 64, 3, stride=1, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(64, 32, 3, stride=1, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(32, out_channels, 3, stride=1, padding=1),
-            nn.LeakyReLU(0.1))
-
-        # Initialize weights
-        for m in self.modules():
-            if isinstance(m, nn.ConvTranspose2d) or isinstance(m, nn.Conv2d):
-                nn.init.kaiming_normal_(m.weight.data)
-                m.bias.data.zero_()
-
-
-        pass
-
-    
-
-    def load_pretrained_model(self ) -> None:
-        ## This loads the parameters saved in bestmodel.pth into the model
-        pass
+class Net(nn.Module):
+    def __init__(self,in_ch, m, k):
+        super().__init__()
         
-    def train ( self , train_input , train_target ) -> None:
-        # : train_input : tensor of size (N , C , H , W ) containing a noisy version of the images
-        # : train_target : tensor of size (N , C , H , W ) containing another noisy version of the same images, which only differs from the input by their noise .
-        pass
+        self.conv1 = nn.Conv2d(in_ch, m, kernel_size = k, stride=1, padding=0)
+        self.conv2 = nn.Conv2d(m, m*2, kernel_size = k, stride=1, padding=0)
+        self.tconv1 = nn.ConvTranspose2d(m*2, m, kernel_size = k, stride=1, padding=0)
+        self.tconv2 = nn.ConvTranspose2d(m, in_ch, kernel_size = k, stride=1, padding=0)
+        
+        self.relu = nn.ReLU()
+        self.sigmoid = nn.Sigmoid()
+        
+    def forward(self, x):
+        x = self.relu(self.conv1(x))
+        x = self.relu(self.conv2(x))
+        x = self.relu(self.tconv1(x))
+        x = self.sigmoid(self.tconv2(x))
+        return x
+        
+class Model():
+    def __init__(self):
+        self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+        self.batch_size, self.nb_epochs = 50, 1000
+        self.in_ch = 3
+        self.m = 16
+        self.k = 3
+        
+        # Instantiate model
+        self.model = Net(self.in_ch, self.m, self.k)
+        self.model.to(self.device)
+        
+        # Optimizer
+        self.optimizer = torch.optim.Adam(self.model.parameters(), lr = 1e-2)
+        
+        # Loss function
+        self.mse = nn.MSELoss()
+        
+        # Scheduler
+        self.scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(self.optimizer, 'min', factor = 0.5, threshold  = 1e-10)
+        
+    def load_pretrained_model(self):
+        torch.load(self.model,'bestmodel.pth')
     
-    def predict ( self , test_input ) -> torch.Tensor:
-        # :test_input: tensor of size ( N1 , C , H , W ) that has to be denoised by the trained or the loaded network .
-        # : returns a tensor of the size ( N1 , C , H , W )
-        pass
+    def train(self, train_input, train_target, num_epochs):
+        input = train_input.to(self.device).type(torch.float).split(self.batch_size)
+        target = train_target.to(self.device).type(torch.float).split(self.batch_size)
+#         valid_inp = noisy_imgs.to(device).type(torch.float)
+#         valid_target = clean_imgs.to(device).type(torch.float)
+        
+        loss_train = torch.zeros(num_epochs, device = self.device)
+        loss_valid = torch.zeros(num_epochs, device = self.device)
+        
+        for e in tqdm(range(num_epochs)):
+            
+            self.model.train()
+            for i in range(len(input)):
+                output = self.model(input[i])
+                loss_batch = self.mse(output, target[i])
+                loss_train[e] += loss_batch
+                self.optimizer.zero_grad()
+                loss_batch.backward()
+                self.optimizer.step()
+
+    def train_with_scheduler(self, train_input, train_target, valid_input, valid_target, num_epochs):
+        input = train_input.to(self.device).type(torch.float).split(self.batch_size)
+        target = train_target.to(self.device).type(torch.float).split(self.batch_size)
+        valid_inp = valid_input.to(self.device).type(torch.float)
+        valid_target = valid_target.to(self.device).type(torch.float)
+        
+        loss_train = torch.zeros(num_epochs, device = self.device)
+        loss_valid = torch.zeros(num_epochs, device = self.device)
+        
+        for e in tqdm(range(num_epochs)):
+            
+            self.model.train()
+            for i in range(len(input)):
+                output = self.model(input[i])
+                loss_batch = self.mse(output, target[i])
+                loss_train[e] += loss_batch
+                loss_batch.backward()
+                self.optimizer.zero_grad()
+                self.optimizer.step()
+                
+            self.model.eval()
+            output = self.model(valid_inp)
+            loss_valid[e] = self.mse(valid_inp, valid_target)
+            
+            self.scheduler.step(loss_valid[e])
+            
+    def predict(self, test_input):
+        
+        return self.model(test_input.to(self.device))
